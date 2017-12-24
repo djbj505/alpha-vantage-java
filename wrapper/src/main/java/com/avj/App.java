97f30fb2eff31051aef6cc19ef8a773d2f12b986
Bryce <brycechae@users.noreply.github.com>
2017-12-24 13:55:56 -0800

    Update App.java
    Removing unnecessary imports from app.java

--- wrapper/src/main/java/com/avj/App.java
+++ wrapper/src/main/java/com/avj/App.java
@@ -1,16 +1,5 @@
 package com.avj;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import com.avj.utils.AlphaVantageRequest;
-
-/**
- * Hello world!
- *
- */
 public class App {
 	
 	
